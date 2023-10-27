fc60c2bdb55e306aaa0c838f203f5474eb136e7e
PedroGaya <pedrohenriquegaya@gmail.com>
2023-10-27 15:33:24 -0300

    update elo code to return pair of Rating

--- src/elo.ts
+++ src/elo.ts
@@ -1,4 +1,4 @@
-import { EloParams, EloRating, Match } from "./types";
+import { EloParams, EloRating, Match, Rating } from "./types";
 
 export class Elo {
     defaultRating: number;
@@ -16,35 +16,35 @@ export class Elo {
         };
     }
 
-    public updateRating(m: Match): { p1: EloRating; p2: EloRating } {
+    public updateRating(m: Match): { p1: Rating; p2: Rating } {
         const { players, score } = m;
 
-        const P1 = players[0];
-        const P2 = players[1];
+        const p1 = players[0];
+        const p2 = players[1];
 
         const p1s = score; // Actual score for p1
         const p2s = 1 - score; // Actual score for p2
 
-        const q1 = 10 ** (P1.elo.rating / 480);
-        const q2 = 10 ** (P2.elo.rating / 480);
+        const q1 = 10 ** (p1.elo.rating / 480);
+        const q2 = 10 ** (p2.elo.rating / 480);
 
         const p1e = q1 / (q1 + q2); // Expected score for p1
         const p2e = q2 / (q2 + q1); // Expected score for p2
 
-        const p1k = this.getK(P1.elo.rating, p1s);
-        const p2k = this.getK(P2.elo.rating, p2s);
+        const p1k = this.getK(p1.elo.rating, p1s);
+        const p2k = this.getK(p2.elo.rating, p2s);
 
-        const new_p1r = P1.elo.rating + p1k * (p1s - p1e);
-        const new_p2r = P2.elo.rating + p2k * (p2s - p2e);
+        const new_p1r = p1.elo.rating + p1k * (p1s - p1e);
+        const new_p2r = p2.elo.rating + p2k * (p2s - p2e);
 
         return {
             p1: {
-                rating: new_p1r,
-                k_value: p1k,
+                glicko: { ...p1.glicko },
+                elo: { rating: new_p1r, k_value: p1k },
             },
             p2: {
-                rating: new_p2r,
-                k_value: p2k,
+                glicko: { ...p2.glicko },
+                elo: { rating: new_p2r, k_value: p2k },
             },
         };
     }
