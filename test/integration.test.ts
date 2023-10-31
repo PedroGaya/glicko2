21b85f32f56626470c31c20df6765d4109bfbc52
PedroGaya <pedrohenriquegaya@gmail.com>
2023-10-31 18:15:37 -0300

    integration tests!

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,111 +1,143 @@
+import { test, expect, describe } from "bun:test";
+
 import { Ladder } from "../src/classes/ladder";
 import { User } from "../src/classes/user";
+import { Match } from "../src/types";
 
-import { prisma } from "../libs/prisma";
+import {
+    ladderTestParams,
+    getTestLadder,
+    getTestPlayers,
+    setTestMatches,
+    getTestMatches,
+} from "./setup";
 
-await prisma.rating.deleteMany();
-await prisma.match.deleteMany();
-await prisma.user.deleteMany();
-await prisma.ladder.deleteMany();
-await prisma.game.deleteMany();
+describe("Creating the basic data entities.", async () => {
+    test("Create a ladder", async () => {
+        const ladder = getTestLadder();
+        const params = ladderTestParams;
 
-const ladder = await Ladder.build({
-    game: "Glickman",
-    name: "Glicko2 Testing Ladder",
-    elo: {
-        defaultRating: 1000,
-        defaultK: 50,
-    },
-    glicko: {
-        defaultRating: 1500,
-        defaultRatingDeviation: 200,
-        defaultVolatility: 0.06,
-        tau: 0.5,
-    },
-    ratingPeriod: {
-        games: 3,
-        hours: 24,
-    },
-    matches: [],
-    players: [],
-});
+        expect(ladder).toBeInstanceOf(Ladder);
+        expect(ladder.id).toBeString();
 
-const player = await User.build({
-    name: "player",
-    ratings: [],
-});
-const op1400 = await User.build({
-    name: "1400",
-    ratings: [
-        {
-            ladderId: ladder.id,
-            rating: {
-                elo: {
-                    rating: 1000,
-                    k_value: 50,
-                },
-                glicko: {
-                    rating: 1400,
-                    deviation: 30,
-                    volatility: 0.06,
-                },
+        expect(ladder.elo.defaultRating).toBe(params.elo.defaultRating);
+        expect(ladder.glicko.defaultRating).toBe(params.glicko.defaultRating);
+        expect(ladder.glicko.defaultRatingDeviation).toBe(
+            params.glicko.defaultRatingDeviation
+        );
+        expect(ladder.glicko.defaultVolatility).toBe(
+            params.glicko.defaultVolatility
+        );
+        expect(ladder.glicko.tau).toBe(params.glicko.tau);
+
+        expect(ladder.matches).toBeArrayOfSize(0);
+        expect(ladder.players).toBeArrayOfSize(0);
+    });
+
+    test("Create players", async () => {
+        const ladder = getTestLadder();
+        const { player, op1400, op1550, op1700 } = getTestPlayers();
+
+        expect(player).toBeInstanceOf(User);
+        expect(player.id).toBeString();
+
+        await ladder.registerPlayer(player);
+        await ladder.registerPlayer(op1400);
+        await ladder.registerPlayer(op1550);
+        await ladder.registerPlayer(op1700);
+
+        expect(player.getRating(ladder.id)).toEqual({
+            elo: ladder.elo.getNewRating(),
+            glicko: ladder.glicko.getNewRating(),
+        });
+        expect(op1700.getRating(ladder.id)).toEqual({
+            elo: {
+                rating: 1000,
+                k_value: 50,
             },
-        },
-    ],
-});
-const op1550 = await User.build({
-    name: "1550",
-    ratings: [
-        {
-            ladderId: ladder.id,
-            rating: {
-                elo: {
-                    rating: 1000,
-                    k_value: 50,
-                },
-                glicko: {
-                    rating: 1550,
-                    deviation: 100,
-                    volatility: 0.06,
-                },
+            glicko: {
+                rating: 1700,
+                deviation: 300,
+                volatility: 0.06,
             },
-        },
-    ],
+        });
+
+        expect(ladder.players).toBeArrayOfSize(4);
+    });
 });
-const op1700 = await User.build({
-    name: "1700",
-    ratings: [
-        {
-            ladderId: ladder.id,
-            rating: {
-                elo: {
-                    rating: 1000,
-                    k_value: 50,
-                },
-                glicko: {
-                    rating: 1700,
-                    deviation: 300,
-                    volatility: 0.06,
-                },
+
+describe("Play some matches and calculate rating updates", async () => {
+    test("Start matches", async () => {
+        const ladder = getTestLadder();
+        const { player, op1400, op1550, op1700 } = getTestPlayers();
+        const matches = setTestMatches([
+            await ladder.startMatch(player, op1400),
+            await ladder.startMatch(player, op1550),
+            await ladder.startMatch(op1700, player),
+        ]);
+
+        expect(matches[0].score).toBeNull();
+        expect(matches[0].hasRatedPlayerOne).toEqual({
+            elo: false,
+            glicko: false,
+        });
+
+        expect(ladder.matchesOngoing).toBeArrayOfSize(3);
+    });
+
+    test("Finish matches.", async () => {
+        const ladder = getTestLadder();
+        const matches = getTestMatches();
+
+        const m1 = await ladder.endMatch(matches[0].id, 1);
+        const m2 = await ladder.endMatch(matches[1].id, 0);
+
+        expect(ladder.matchesOngoing).toBeArrayOfSize(1);
+        expect(ladder.matches).toBeArrayOfSize(2);
+
+        const m3 = await ladder.endMatch(matches[2].id, 1);
+
+        setTestMatches([m1, m2, m3]);
+
+        expect(m1.score).toBe(1);
+        expect(m1.hasRatedPlayerOne).toEqual({
+            elo: true,
+            glicko: false,
+        });
+
+        expect(ladder.matchesOngoing).toBeArrayOfSize(0);
+        expect(ladder.matches).toBeArrayOfSize(3);
+    });
+
+    test("Calculate ratings", () => {
+        const ladder = getTestLadder();
+        const { player, op1400, op1550, op1700 } = getTestPlayers();
+
+        const playerRating = {
+            glicko: {
+                rating: 1500,
+                deviation: 200,
+                volatility: 0.06,
             },
-        },
-    ],
+            elo: {
+                rating: 1008.2427703830168,
+                k_value: 27,
+            },
+        };
+
+        const op1700rating = {
+            glicko: {
+                rating: 1700,
+                deviation: 300,
+                volatility: 0.06,
+            },
+            elo: {
+                rating: 1042.1536756016758,
+                k_value: 80,
+            },
+        };
+
+        expect(player.getRating(ladder.id)).toEqual(playerRating);
+        expect(op1700.getRating(ladder.id)).toEqual(op1700rating);
+    });
 });
-
-await ladder.registerPlayer(player);
-await ladder.registerPlayer(op1400);
-await ladder.registerPlayer(op1550);
-await ladder.registerPlayer(op1700);
-
-let m1 = await ladder.startMatch(player, op1400);
-let m2 = await ladder.startMatch(player, op1550);
-let m3 = await ladder.startMatch(op1700, player);
-
-m1 = await ladder.endMatch(m1.id, 1);
-m2 = await ladder.endMatch(m2.id, 0);
-m3 = await ladder.endMatch(m3.id, 1);
-
-const matches = [m1, m2, m3];
-const updatedRating = await ladder.updateGlicko(player, matches);
-
-console.log(updatedRating);
---
+++ test/setup.ts
@@ -0,0 +1,132 @@
+import { beforeAll } from "bun:test";
+import { prisma } from "../libs/prisma";
+
+import { User } from "../src/classes/user";
+import { Ladder } from "../src/classes/ladder";
+import { Match } from "../src/types";
+
+let player: User | undefined,
+    op1400: User | undefined,
+    op1550: User | undefined,
+    op1700: User | undefined;
+
+let ladder: Ladder | undefined;
+let matches: Match[] = [];
+
+export const ladderTestParams = {
+    game: "Glickman",
+    name: "Glicko2 Testing Ladder",
+    elo: {
+        defaultRating: 1000,
+        defaultK: 50,
+    },
+    glicko: {
+        defaultRating: 1500,
+        defaultRatingDeviation: 200,
+        defaultVolatility: 0.06,
+        tau: 0.5,
+    },
+    ratingPeriod: {
+        games: 3,
+        hours: 24,
+    },
+    matches: [],
+    players: [],
+};
+
+beforeAll(async () => {
+    if (Bun.env.NODE_ENV != "development")
+        throw "Running integration tests in production! Aborting.";
+
+    console.log("Reseting development database...");
+    await prisma.rating.deleteMany();
+    await prisma.match.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.ladder.deleteMany();
+    await prisma.game.deleteMany();
+
+    console.log("Building test ladder...");
+    ladder = await Ladder.build(ladderTestParams);
+
+    console.log("Building test players...");
+    player = await User.build({
+        name: "player",
+        ratings: [],
+    });
+    op1400 = await User.build({
+        name: "1400",
+        ratings: [
+            {
+                ladderId: ladder.id,
+                rating: {
+                    elo: {
+                        rating: 1000,
+                        k_value: 50,
+                    },
+                    glicko: {
+                        rating: 1400,
+                        deviation: 30,
+                        volatility: 0.06,
+                    },
+                },
+            },
+        ],
+    });
+    op1550 = await User.build({
+        name: "1550",
+        ratings: [
+            {
+                ladderId: ladder.id,
+                rating: {
+                    elo: {
+                        rating: 1000,
+                        k_value: 50,
+                    },
+                    glicko: {
+                        rating: 1550,
+                        deviation: 100,
+                        volatility: 0.06,
+                    },
+                },
+            },
+        ],
+    });
+    op1700 = await User.build({
+        name: "1700",
+        ratings: [
+            {
+                ladderId: ladder.id,
+                rating: {
+                    elo: {
+                        rating: 1000,
+                        k_value: 50,
+                    },
+                    glicko: {
+                        rating: 1700,
+                        deviation: 300,
+                        volatility: 0.06,
+                    },
+                },
+            },
+        ],
+    });
+});
+
+export const getTestLadder = () => {
+    if (!ladder) throw "Failed to initialize ladder.";
+    return ladder;
+};
+export const getTestPlayers = () => {
+    if (!player || !op1400 || !op1550 || !op1700)
+        throw "Failed to initialize players.";
+    return { player, op1400, op1550, op1700 };
+};
+
+export const setTestMatches = (newMatches: Match[]) => {
+    matches = newMatches;
+    return matches;
+};
+export const getTestMatches = () => {
+    if (matches.length == 0) throw "Matches haven't been set.";
+    return matches;
+};
