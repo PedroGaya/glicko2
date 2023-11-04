f772e22496dea8a684bedfcbe403803d65fccbc7
PedroGaya <pedrohenriquegaya@gmail.com>
2023-11-04 18:40:52 -0300

    remove useless files

--- src/routes/match.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from "elysia";
-
-const users = new Elysia({ prefix: "/match" });
-
-users.get("/", () => "From matches.");
-users.get("/start", (ctx) => "pong");
-
-export default users;
