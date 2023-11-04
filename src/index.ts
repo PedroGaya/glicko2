4d80ebd4d4c9ff2321d3c9e64db452aae11e0d1f
PedroGaya <pedrohenriquegaya@gmail.com>
2023-11-04 12:47:19 -0300

    proper error handling

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,6 @@ import { APP_HOST, APP_PORT } from "../libs/constants";
 import logger from "../libs/logger";
 import app from "./app";
 
-app.onRequest((ctx) => {
-    const message = {
-        method: ctx.request.method,
-        url: ctx.request.url,
-        body: ctx.request.body,
-    };
-
-    logger.debug(message);
-});
-
-app.onError((ctx) => {
-    const message = {
-        name: ctx.error.name,
-        message: ctx.error.message,
-        cause: ctx.error.cause,
-        stack: ctx.error.stack,
-    };
-    logger.error(message);
-});
-
 app.listen(
     {
         hostname: APP_HOST,
