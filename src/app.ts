3987f8b464de6f9d9899fa307730a53ed34c7c8a
PedroGaya <pedrohenriquegaya@gmail.com>
2023-11-04 12:47:05 -0300

    proper error handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import { Elysia } from "elysia";
 
 import games from "./routes/games";
-import { store } from "../libs/store";
+import users from "./routes/users";
+import logger from "../libs/logger";
 
 const app = new Elysia()
-    .use(store)
+    .onRequest(({ request }) => {
+        const message = {
+            method: request.method,
+            url: request.url,
+            body: request.body,
+        };
+
+        logger.debug(message);
+    })
+    .onError(({ error }) => {
+        const message = {
+            name: error.name,
+            message: error.message,
+            cause: error.cause,
+            stack: error.stack,
+        };
+        logger.error(message);
+        return new Response(error.toString());
+    })
     .get("/", () => "OK")
-    .use(games);
+    .use(games)
+    .use(users);
 
 export default app;
