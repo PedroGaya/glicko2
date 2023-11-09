c99668013c88544b3f80dca2f99441a72cda8190
PedroGaya <pedrohenriquegaya@gmail.com>
2023-11-09 17:19:24 -0300

    exit tests if prisma fails

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import logger from "./logger";
 
 export const prisma = new PrismaClient();
 
 export const prismaCleanup = async () => {
-    await prisma.rating.deleteMany();
-    await prisma.match.deleteMany();
-    await prisma.user.deleteMany();
-    await prisma.ladder.deleteMany();
-    await prisma.game.deleteMany();
+    try {
+        await prisma.rating.deleteMany();
+        await prisma.match.deleteMany();
+        await prisma.user.deleteMany();
+        await prisma.ladder.deleteMany();
+        await prisma.game.deleteMany();
+    } catch (e: unknown) {
+        logger.error(e);
+        process.exit(1);
+    }
 };
